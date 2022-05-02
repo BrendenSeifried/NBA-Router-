8d0792976ac2560cbd28cf6b77c0512a23e674b0
BrendenSeifried <brendenseifried@gmail.com>
2022-05-02 16:56:53 -0700

    added go back button with history.push

--- src/views/CharacterCard.jsx
+++ src/views/CharacterCard.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams, useHistory } from 'react-router-dom';
 import { fetchID } from '../services/fetchAPI';
 
 export default function CharacterCard() {
   const { id } = useParams();
   const [char, setChar] = useState({});
+  const history = useHistory('');
 
   useEffect(() => {
     const fetchCharacter = async () => {
@@ -16,6 +17,10 @@ export default function CharacterCard() {
     fetchCharacter();
   }, [id]);
 
+  const goBack = async () => {
+    history.push(`/`);
+  };
+
   return (
     <article>
       <img alt="image of character" src={char.image} />
@@ -25,6 +30,7 @@ export default function CharacterCard() {
       <h3>
         Specifics: {char.gender} {char.species}
       </h3>
+      <button onClick={goBack}>Return to home page.</button>
     </article>
   );
 }
