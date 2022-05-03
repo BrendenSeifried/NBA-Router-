15081703c310909c83abd1774437dd1c72fa8d6c
BrendenSeifried <brendenseifried@gmail.com>
2022-05-02 17:31:44 -0700

    first test passed

--- src/views/Main.test.jsx
+++ src/views/Main.test.jsx
@@ -1,10 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Main from './Main';
 
 describe('linky stuffs', () => {
-  it('selects link then goes to character details page', () => {
-    render(<Main />);
+  it('selects link then goes to character details page', async () => {
+    render(
+      <MemoryRouter>
+        <Main />
+      </MemoryRouter>
+    );
     screen.getByText(/loading/i);
     screen.debug();
+
+    const testOne = await screen.findByText('Name: Rick Sanchez (Alive)');
+    expect(testOne).toBeInTheDocument();
   });
 });
